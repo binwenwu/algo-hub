f73e97ebb45410461210a312e82004d9bac39074
NerdNinja <binwenwu@whu.edu.cn>
2025-08-19 13:04:19 +0800

    feat: Solve 1 problems

--- leetcode/_129.java
+++ leetcode/_129.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class _129 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
 
+        sc.close();
     }
 
     public class TreeNode {
